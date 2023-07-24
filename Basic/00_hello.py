7bbf9950cb7ea91d7eed919854bcb5d59f5f48d6
leandro <yleandrovarela@gmail.com>
2023-07-24 17:06:02 -0500

    initial commit

---
+++ Basic/00_hello.py
@@ -0,0 +1,11 @@
+# Comentarios en Python
+"""
+Comentario de 
+multiples linesas
+"""
+print('Hello word')
+
+# Consultar el tipo de dato
+print(type('Dato string')) # Tipo str
+print(type(5)) # Tipo int
+
---
+++ Basic/01_variables.py
@@ -0,0 +1,7 @@
+# Variables
+
+my_variable = 'My String Variable'
+print(my_variable)
+print(my_variable, 'Hola')
+
+#1:03
